c4ce3a33c3a6d45c76f9b9493ae50ffd7d445c75
niccobal <niccolo.baldelli@gmail.com>
2020-04-23 14:23:46 +0200

    Version updated for the use with dictionaries. Solves the memory error problems. Should be extended also for bosons

--- ImpuritiesDiag.py
+++ ImpuritiesDiag.py
@@ -10,7 +10,9 @@ from scipy import sparse as spr
 from scipy.sparse import linalg
 import itertools
 from numba import njit,jit
+import time
 
+@njit
 # calculate logarithm of factorial
 def logfac(num):
     if (num==0):
@@ -25,8 +27,8 @@ def LLL(x,y,m):
     return np.sqrt(1/(2*np.pi))*2**(-m/2)*np.exp(-logfac(m)/2)*np.exp(-(x**2+y**2)/4)*(x+1j*y)**m
 
 #calculate pseudopotential matrix elements 
-#(here only for two pseudopotentials vo and v1, at filling 1/5 we need also v2 and v3, different code)
-
+#(here only for two pseudopotentials vo and v1, at filling 1/5 we need also v2 and v3)
+@njit
 def vhal(m1,m2,m3,m4,v0,v1,v2,v3):
     #define the haldane pseudopotentials
     l=4
@@ -47,7 +49,7 @@ def vhal(m1,m2,m3,m4,v0,v1,v2,v3):
                     if (ll>=(m1-k))and(m1-k>=0):        
                         A = logfac(n)+logfac(ll)-logfac(k)-logfac(n-k)-logfac(m1-k)-logfac(ll-m1+k)
                         B = B+np.exp(A)*(-1)**(ll-m1+k);
-                C1=(logfac(m1)+logfac(m2)-logfac(n)-logfac(ll))/2-(n+ll)*(np.log(2)/2)+np.log(np.abs(B))+1j*np.pi*np.heaviside(B,0)
+                C1=(logfac(m1)+logfac(m2)-logfac(n)-logfac(ll))/2-(n+ll)*(np.log(2)/2)+np.log(np.abs(B))+1j*np.pi*0.5*(np.sign(B)+1)
                 C1 =np.exp(C1)
                 # C_m3,m4^n,l
                 B=0
@@ -55,15 +57,29 @@ def vhal(m1,m2,m3,m4,v0,v1,v2,v3):
                     if  (ll>=(m4-k))and(m4-k>=0):
                         A = logfac(n)+logfac(ll)-logfac(k)-logfac(n-k)-logfac(m4-k)-logfac(ll-m4+k)
                         B = B+np.exp(A)*(-1)**(ll-m4+k)
-                C2 = (logfac(m4)+logfac(m3)-logfac(n)-logfac(ll))/2-(n+ll)*(np.log(2)/2)+np.log(np.abs(B))+1j*np.pi*np.heaviside(B,0)
+                C2 = (logfac(m4)+logfac(m3)-logfac(n)-logfac(ll))/2-(n+ll)*(np.log(2)/2)+np.log(np.abs(B))+1j*np.pi*0.5*(np.sign(B)+1)
                 C2 = np.exp(C2);
             # V
                 out=out+C1*C2*v[i];
-    return round(np.real(np.double(out)),11)
+    return np.real(out)
 
-L=27
+@njit
+def hpspot(lmax):
+    vmaj=np.zeros((lmax+1,lmax+1,lmax+1,lmax+1))
+    vmin=np.zeros((lmax+1,lmax+1,lmax+1,lmax+1))
+    for k1 in range(lmax+1):
+        for k2 in range(lmax+1):
+            for k3 in range(lmax+1):
+                for k4 in range(lmax+1):
+                    vmaj[k1,k2,k3,k4]=vhal(k1,k2,k3,k4,0,1,0,0)
+                    vmin[k1,k2,k3,k4]=vhal(k1,k2,k3,k4,1,0,0,0)
+    return vmaj,vmin
+
+tic=time.time()
+
+L=22
 Ne=4
-Nh=2
+Nh=1
 
 gee=1; geh=1; we=1; wh=1
 lmax=3*(Ne-1)+7; #truncation of single particle angumom WHY
@@ -87,8 +103,8 @@ if (Nh==0):
 larr=np.arange(0,lmax+1)
 allchoices=np.array(list(itertools.combinations(larr,Ne)))
 
-ebase=np.zeros((len(allchoices),d),dtype=np.int32) #python cannot create dynamical arrays
-elist=np.zeros(((2**allchoices[-1,-1])+2**(Lmin+d-allchoices[-1,-1]),2)) 
+ebase={}
+elist={}
 
 for i in range(len(allchoices[:,0])):
     Le=sum(allchoices[i,:]); # angular momentum of a given configuration i
@@ -98,8 +114,8 @@ for i in range(len(allchoices[:,0])):
         num=0
         for j in range(Ne):
             num=num+2.**allchoices[i,j]
-        ebase[De[sector]-1,sector]=int(num) #the first index goes as long as I have states to put!!
-        elist[int(num),:]=[sector,De[sector]] #understand how I could do this
+        ebase.update({(De[sector]-1,sector):int(num)})
+        elist.update({int(num):(sector,De[sector])})
 
 # CONFIGURATIONS FOR MINORITY PARTICLES
 # (Essentially the same as for majority particles, as we take both of them
@@ -107,8 +123,8 @@ for i in range(len(allchoices[:,0])):
 
 if (Nh>0):  
     allchoices=np.array(list(itertools.combinations(larr,Nh)))
-    hbase=np.zeros((len(allchoices),d)) #python cannot create dynamical arrays
-    hlist=np.zeros((sum(2**(allchoices[-1,:]))+1,2)) 
+    hbase={} #python cannot create dynamical arrays
+    hlist={} 
     for i in range(len(allchoices)):
         Lh=sum(allchoices[i,:])
         sector=L-Lh-Lmin; #why this? becaus it is exactly Le-Lmin
@@ -117,27 +133,20 @@ if (Nh>0):
             num=0
             for j in range(Nh):
                 num=num+2.**allchoices[i,j]
-            hbase[Dh[sector]-1,sector]=int(num)
-            hlist[int(num),:]=[sector,Dh[sector]]
+            hbase.update({(Dh[sector]-1,sector):int(num)})
+            hlist.update({int(num):(sector,Dh[sector])})
             
 D=De*Dh; # This is a vector with Hilbert space dimension in each sector, being the product De(sector) X Dh(sector)
 Dtot=sum(D) # summing over all sector: total Hilbert space dimension
 print(Dtot)
 print(d)
 
-# this is a vector with powers of 2, we will use it to relate
-# configurations to binary numbers
+# this is a vector with powers of 2, we will use it to relate  configurations to binary numbers
 pots=2.**(lmax-np.arange(0,lmax+1))
 
 #% that turns out to be faster than calling the function whenever we need it
-vmaj=np.zeros((lmax+1,lmax+1,lmax+1,lmax+1))
-vmin=np.zeros((lmax+1,lmax+1,lmax+1,lmax+1))
-for k1 in range(lmax+1):
-    for k2 in range(lmax+1):
-        for k3 in range(lmax+1):
-            for k4 in range(lmax+1):
-                vmaj[k1,k2,k3,k4]=vhal(k1,k2,k3,k4,0,1,0,0)
-                vmin[k1,k2,k3,k4]=vhal(k1,k2,k3,k4,1,0,0,0)
+
+vmaj,vmin=hpspot(lmax)
 
 # matrix elements for e-e interactions
 iee=[]; jee=[]; vee=[];
@@ -153,18 +162,8 @@ for sec in range(d): # loop over all sectors
         
         if (Nh>0): # if we have no impurities, the impurity has not been defined, therefore the "if" 
             hbin=np.fromstring(np.binary_repr(int(hbase[ih,sec])), dtype='S1').astype(int)
-            hbin=np.pad(hbin,(lmax-len(hbin)+1,0),'constant',constant_values=(0,0))
-            
-    #        % this transforms the number of the state (saved in hbase) into the binary number which characterizes the occupation 
-    #        % It is a binary with lmax+1 digits (for smaller numbers, the first digits are zero)
-    #        % the command -'0' transform the number into a vector, 
-    #        % i.e 101-'0'  -->  (1,0,1)
-            
+            hbin=np.pad(hbin,(lmax-len(hbin)+1,0),'constant',constant_values=(0,0))            
             hoccs=lmax-np.flip(np.argwhere(hbin)[:,0]) 
-    #        % e.g. (1,0,0,1,1) -> (0,1,4) [we have chosen the smaller levels to
-    #        % be on the right, so level on the right has angular momentum 0,
-    #        % increasing to lmax=4, being the level on the left]
-            
         for ie in range(De[sec]):  # loop over all majority configurations
             ebin=np.fromstring(np.binary_repr(int(ebase[ie,sec])), dtype='S1').astype(int)
             ebin=np.pad(ebin,(lmax-len(ebin)+1,0),'constant',constant_values=(0,0))
@@ -172,14 +171,6 @@ for sec in range(d): # loop over all sectors
             eemps=np.setdiff1d(larr,eoccs);  # empty angular momentum levels
 
             inn=(sum(D[:sec])+(ih)*De[sec]+ie).astype(np.int32); #penso che sto numero sia abbastanza arbitrario
-#            % here we define a unique number of the state in the matrix, 
-#            % i.e. in a combined basis for majority and impurity.
-#            % obviously, this number is between 1 and D, and it's uniquely
-#            % given by ie, ih, sec
-                         
-#            % diagonal parts of the matrix from the harmonic trap - depends
-#            % only on the total angular momentum of majority and impurity
-#            % particles
             etrap.append(sec+Lmin)
             htrap.append(L-Lmin-sec)
             
@@ -209,7 +200,7 @@ for sec in range(d): # loop over all sectors
                         v2=sig*(vmaj[ll1,ll2,l1,l2]-vmaj[ll1,ll2,l2,l1]-vmaj[ll2,ll1,l1,l2]+vmaj[ll2,ll1,l2,l1]);
     #                    % what is the position number of this newebin
     #                    % configuration ?? Look up in elist:
-                        ieout=int(elist[int(np.dot(newebin,pots)),1]);
+                        ieout=int(elist[int(np.dot(newebin,pots))][1]);
     #                    % This is the number of the new state in the matrix
     #                    % (i.e. combined basis of majority and impurity)
                         outt=(sum(D[:sec])+(ih)*De[sec]+ieout-1).astype(np.int32) 
@@ -238,14 +229,17 @@ for sec in range(d): # loop over all sectors
                                    dd=len(np.where(occs2<l3)[0]) # how many occupied levels must impurity creator pass
                                    sig=sig*(-1)**(cc+dd)
                                    v2=sig*vmin[l4,l3,l2,l1] # vhal(l4,l3,l2,l1,1,0);
-                                   ieout=int(elist[int(np.dot(newebin,pots)),1])
-                                   ihout=int(hlist[int(np.dot(newhbin,pots)),1])
-                                   secout=int(elist[int(np.dot(newebin,pots)),0])
+                                   ieout=int(elist[int(np.dot(newebin,pots))][1])
+                                   ihout=int(hlist[int(np.dot(newhbin,pots))][1])
+                                   secout=int(elist[int(np.dot(newebin,pots))][0])
                                    
                                    outt=(sum(D[:(secout)])+(ihout-1)*De[secout]+ieout-1).astype(np.int32);
                                    ieh.append(inn) # this is the index for the incoming state
                                    jeh.append(outt) # this is the inces for the outcoming state
                                    veh.append(v2) # this is the interaction strength of the scatter process connecting the states
+
+toc=time.time()
+print(toc-tic)
 
 # Build the sparse matrices from the lists we have produced
 vehmat=spr.coo_matrix((veh,(ieh,jeh)),shape=(Dtot,Dtot))
@@ -275,16 +269,14 @@ for sec in range(d):
         varl[e]=varl[e]+((Lmin+sec)**2)*np.dot(vecs[i:i+D[sec],e],vecs[i:i+D[sec],e]);
     i=i+D[sec];
 std=np.sqrt(varl-Lel**2)
-#
-#np.savetxt('valsfer'+str(L)+str(Ne)+str(Nh),vals)
-#np.savetxt('angmfer'+str(L)+str(Ne)+str(Nh),L-Lel)
-#np.savetxt('stdevfer'+str(L)+str(Ne)+str(Nh),std)
+
+np.savetxt('valsfer'+str(L)+str(Ne)+str(Nh),vals)
+np.savetxt('angmfer'+str(L)+str(Ne)+str(Nh),L-Lel)
+np.savetxt('stdevfer'+str(L)+str(Ne)+str(Nh),std)
 print(vals[0])
 print((L-Lel)[0])
 print(std[0])
 
-def mee(m,nu):
-    return (m+nu)/(1-nu)
 
-def stod(m,nu):
-    return np.sqrt(((1+m)*nu)/(nu-1)**2)
+
+
